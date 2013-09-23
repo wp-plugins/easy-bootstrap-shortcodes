4912e33786cc95d6a7f0d90724188b45c6a1a378
oscitas <oscitas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-23 14:56:11 +0000

    Add new functionality
    git-svn-id: https://plugins.svn.wordpress.org/easy-bootstrap-shortcodes/trunk@777310 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcode/icon/icon_plugin.js
+++ shortcode/icon/icon_plugin.js
@@ -1,21 +1,21 @@
 (function() {
-    tinymce.create('tinymce.plugins.oscitasButtons', {
+    tinymce.create('tinymce.plugins.oscitasIcon', {
         init : function(ed, url) {
-            ed.addButton('oscitasbuttons', {
-                title : 'Button Shortcode',
+            ed.addButton('oscitasicon', {
+                title : 'Icon Shortcode',
                 image : url+'/icon.png',
                 onclick : function() {
-                    create_oscitas_button();
+                    create_oscitas_icon();
                     jQuery.fancybox({
                         'autoSize':false,
                         'autoWidth':false,
                         'fitToView':false,
                         'height':'auto',
-                        'topRatio':0.1,
-                        'width':800,
+                        'topRatio':0.15,
+                        'width':600,
                         'type' : 'inline',
-                        'title' : 'Button Shortcode',
-                        'href' : '#oscitas-form-button',
+                        'title' : 'Icon Shortcode',
+                        'href' : '#oscitas-form-icon',
                         helpers:  {
                             title : {
                                 type : 'over',
@@ -31,7 +31,7 @@
         },
         getInfo : function() {
             return {
-                longname : "Button Shortcode",
+                longname : "Icon Shortcode",
                 author : 'Oscitas Themes',
                 authorurl : 'http://www.oscitasthemes.com/',
                 infourl : 'http://www.oscitasthemes.com/',
@@ -39,52 +39,21 @@
             };
         }
     });
-    tinymce.PluginManager.add('oscitasbuttons', tinymce.plugins.oscitasButtons);
+    tinymce.PluginManager.add('oscitasicon', tinymce.plugins.oscitasIcon);
 })();
 
-function create_oscitas_button(){
-    if(jQuery('#oscitas-form-button').length){
-        jQuery('#oscitas-form-button').remove();
+function create_oscitas_icon(){
+    if(jQuery('#oscitas-form-icon').length){
+        jQuery('#oscitas-form-icon').remove();
     }
     // creates a form to be displayed everytime the button is clicked
     // you should achieve this using AJAX instead of direct html code like this
-    var form = jQuery('<div id="oscitas-form-button"><table id="oscitas-table" class="form-table">\
-			<tr>\
-				<th><label for="oscitas-button-style">Style:</label></th>\
-				<td><select name="type" id="oscitas-button-style">\
-					<option value="btn-default">Simple</option>\
-					<option value="btn-primary">Primary</option>\
-					<option value="btn-success">Success</option>\
-					<option value="btn-info">Information</option>\
-					<option value="btn-warning">Warning</option>\
-					<option value="btn-danger">Danger</option>\
-					<option value="btn-link">Link</option>\
-				</select><br />\
-				</td>\
-			</tr>\
-			<tr>\
-				<th><label for="oscitas-button-size">Size:</label></th>\
-				<td><select name="type" id="oscitas-button-size">\
-					<option value="btn-lg">Large</option>\
-					<option value="btn-sm">Small</option>\
-					<option value="btn-xs">Ex-small</option>\
-				</select><br />\
-				</td>\
-			</tr>\
-                        <tr>\
-				<th><label for="oscitas-button-type">Type:</label></th>\
-				<td><select name="type" id="oscitas-button-type">\
-					<option value="link">Link</option>\
-					<option value="button">Button</option>\
-				</select><br />\
-				</td>\
-			</tr>\
+    var form = jQuery('<div id="oscitas-form-icon"><table id="oscitas-table" class="form-table">\
                         <tr>\
                         <th><label for="oscitas-heading-icon">Select Icon:</label></th>\
-				<td><div id="click_icon_list_button" class="oscitas-icon-div"><span id="osc_show_icon_button"></span><span class="show-drop"></span></div><input type="hidden" id="osc_icon_class_val_button" value="">\
-                    <div id="osc_show_iconlist_button" class="oscitas-icon" style="display:none;width:100%"><ul name="oscitas-heading-icon_button" id="oscitas-heading-icon_button" class="oscitas-icon-ul">\
-                     <li data-value="" class="glyphicon">N</li>\
-                    <li data-value="glyphicon-adjust" class="glyphicon glyphicon-adjust"> </li>\
+				<td><div id="click_icon_list_icon" class="oscitas-icon-div"><span id="osc_show_icon_icon"></span><span class="show-drop"></span></div><input type="hidden" id="osc_icon_class_val_icon" value="glyphicon-adjust">\
+                    <div id="osc_show_iconlist_icon" class="oscitas-icon" style="display:none;width:100%"><ul name="oscitas-heading-icon_icon" id="oscitas-heading-icon_icon" class="oscitas-icon-ul">\
+                    <li data-value="glyphicon-adjust" class="glyphicon glyphicon-adjust osc_icon_selected_button"> </li>\
                     <li data-value="glyphicon-align-center" class="glyphicon glyphicon-align-center"> </li>\
                     <li data-value="glyphicon-align-justify" class="glyphicon glyphicon-align-justify"> </li>\
                     <li data-value="glyphicon-align-left" class="glyphicon glyphicon-align-left"> </li>\
@@ -287,131 +256,57 @@ function create_oscitas_button(){
         </ul></div>\
 				</td>\
 			</tr>\
-                        <tr>\
-				<th><label for="oscitas-button-iconalign">Icon Alignment:</label></th>\
-				<td><select name="type" id="oscitas-button-iconalign">\
-					<option value="left">Left</option>\
-					<option value="right">Right</option>\
-				</select><br />\
-				</td>\
-			</tr>\
-			<tr>\
-				<th><label for="oscitas-table-rows">Make block</label></th>\
-				<td>\
-				    <input type="checkbox" id="oscitas-button-block">\
-                    <br />\
-				</td>\
-			</tr>\
-			<tr>\
-				<th><label for="oscitas-button-title">Title:</label></th>\
-				<td><input type="text" name="title" id="oscitas-button-title" value="Button"/><br />\
-				</td>\
-			</tr>\
-			<tr id="tr-button-link">\
-				<th><label for="oscitas-button-link">Link</label></th>\
-				<td><input type="text" name="link" id="oscitas-button-link" value="http://www.yoursitename.com" /><br />\
-				</td>\
-			</tr>\
-			<tr id="tr-button-newwindow">\
-				<th><label for="oscitas-table-rows">Open in new window</label></th>\
-				<td>\
-				    <input type="checkbox" id="oscitas-button-target">\
-                    <br />\
-				</td>\
-			</tr>\
                          <tr>\
-				<th><label for="oscitas-button-class">Custom Class:</label></th>\
-				<td><input type="text" name="line" id="oscitas-button-class" value=""/><br />\
+				<th><label for="oscitas-icon-class">Custom Class:</label></th>\
+				<td><input type="text" name="line" id="oscitas-icon-class" value=""/><br />\
 				</td>\
 			</tr>\
 		</table>\
 		<p class="submit">\
-			<input type="button" id="oscitas-button-submit" class="button-primary" value="Insert Button" name="submit" />\
+			<input type="button" id="oscitas-icon-submit" class="button-primary" value="Insert icon" name="submit" />\
 		</p>\
 		</div>');
 		
     var table = form.find('table');
     jQuery('.glyphicon').css('display','inline');
     form.appendTo('body').hide();
-    table.find('#click_icon_list_button').click(function(){
-        if(!jQuery(this).hasClass('osc_icon_showing_button')){
-            jQuery(this).addClass('osc_icon_showing_button')
-            table.find('#osc_show_iconlist_button').show();
+    var t= table.find('#osc_icon_class_val_icon').val();
+    table.find('#osc_show_icon_icon').removeClass().addClass('glyphicon').addClass(t);
+    table.find('#click_icon_list_icon').click(function(){
+        if(!jQuery(this).hasClass('osc_icon_showing_icon')){
+            jQuery(this).addClass('osc_icon_showing_icon')
+            table.find('#osc_show_iconlist_icon').show();
         } else{
-            jQuery(this).removeClass('osc_icon_showing_button')
-            table.find('#osc_show_iconlist_button').hide();
+            jQuery(this).removeClass('osc_icon_showing_icon')
+            table.find('#osc_show_iconlist_icon').hide();
         }
     });
-    table.find('#oscitas-heading-icon_button li').click(function(){
+    table.find('#oscitas-heading-icon_icon li').click(function(){
         var val=jQuery(this).attr('data-value');
-        table.find('#oscitas-heading-icon_button li').removeClass('osc_icon_selected_button');
+        table.find('#oscitas-heading-icon_icon li').removeClass('osc_icon_selected_button');
         jQuery(this).addClass('osc_icon_selected_button');
-        table.find('#osc_show_iconlist_button').hide();
-        table.find('#osc_show_icon_button').removeClass().addClass('glyphicon').addClass(val);
-        table.find('#osc_icon_class_val_button').val(val);
+        table.find('#osc_show_iconlist_icon').hide();
+        table.find('#osc_show_icon_icon').removeClass().addClass('glyphicon').addClass(val);
+        table.find('#osc_icon_class_val_icon').val(val);
     })
 
-    table.find('#oscitas-button-type').change(function(){
-        var abc = jQuery(this).val();
-        if('link' == abc){
-            jQuery("#tr-button-link").show();
-            jQuery("#tr-button-newwindow").show();
-        }else{
-            jQuery("#tr-button-link").hide();
-            jQuery("#tr-button-newwindow").hide();
-        }
-        jQuery('#oscitas-form-button table tr:visible:even').css('background', '#F0F0F0');
-        jQuery('#oscitas-form-button table tr:visible:odd').css('background', '#DADADD');
-    });
+   
         
 
         
 		
     // handles the click event of the submit button
-    form.find('#oscitas-button-submit').click(function(){
-        // defines the options and their default values
-        // again, this is not the most elegant way to do this
-        // but well, this gets the job done nonetheless
-        var options;
-        var type = table.find('#oscitas-button-type').val();
-        if(type=='button'){
-            options = { 
-                'title'       : 'osCitas'
-            };
+    form.find('#oscitas-icon-submit').click(function(){
+        var cusclass;
+        if(table.find('#oscitas-icon-class').val()!=''){
+            cusclass= ' class="'+table.find('#oscitas-icon-class').val()+'"';
         }
         else{
-            options = { 
-                'title'       : 'osCitas',
-                'link'        : ''
-            };
+            cusclass='';
         }
-        var cusclass='',icon='';
-        if(table.find('#oscitas-button-class').val()!=''){
-            cusclass= ' class="'+table.find('#oscitas-button-class').val()+'"';
-        }
-        if(table.find('#osc_icon_class_val_button').val()!=''){
-            icon= ' icon="'+table.find('#osc_icon_class_val_button').val()+'" ';
-        }
-       
-        var shortcode = '[button'+cusclass;
-        
-        shortcode += ' style="'+table.find('#oscitas-button-style').val();
-        shortcode += ' '+table.find('#oscitas-button-size').val();
-        shortcode += table.find('#oscitas-button-block').prop('checked')? ' btn-block': '';
-        shortcode += '" ';
-        shortcode += icon;
-        shortcode += ' align="'+table.find('#oscitas-button-iconalign').val()+'" ';
-        shortcode += ' type="'+type+'" ';
-        if(type!='button'){
-            shortcode += ' target="'+(table.find('#oscitas-button-block').prop('checked')? 'true': 'false')+ '" ';
-        }
-        for( var index in options) {
-            var value = table.find('#oscitas-button-' + index).val();
-            //            var value = table.find('#oscitas-button-' + index).val();
-            shortcode += ' ' + index + '="' + value + '"';
-        }
-			
-        shortcode += ']';
+        var icon = table.find('#osc_icon_class_val_icon').val();
+        var  shortcode='';
+        shortcode='[icon type="'+icon+'"'+cusclass+']'
 			
         // inserts the shortcode into the active editor
         tinyMCE.activeEditor.execCommand('mceInsertContent', 0, shortcode);
