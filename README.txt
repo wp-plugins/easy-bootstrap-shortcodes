e69bc88c5ddef2045ff2ebbd1190a80e2ae78bb6
oscitas <oscitas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-06 06:19:03 +0000

    Fixed few issues
    git-svn-id: https://plugins.svn.wordpress.org/easy-bootstrap-shortcodes/tags/3.7@783366 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,115 @@
+=== Easy Bootstrap Shortcode ===
+Contributors: osCitas Websolution Pvt. Ltd.
+Link: http://www.osCitasthemes.com
+Tags: Wordpress Shortcodes, Wordpress Bootstrap, Wordpress Bootstrap Shortcode, Bootstrap, Responsive pages, Editor plugin, Bootstrap Shortcode, TinyMCE
+Requires at least: 3.0
+Tested up to: 3.6.1
+Stable tag: 3.7
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy Bootstrap Shortcode enable you to add bootstrap 3.0 styles in your pages, post and custom post in simplest manner.
+
+== Description ==
+
+Easy Bootstrap Shortcode is bootstrap 3.0 compatible plugin which add icons to wordpress editor (tinyMCE Shortcode Buttons) and one can add bootstrap style to their website just by clicking on tinyMCE  editor icon.
+
+http://www.youtube.com/watch?v=8T0IC1PsdTw
+
+= For best results use with bootstrap 3.0 responsive theme =
+
+= Features: =
+
+* simplest bootstrap style plugin ever.
+* Bootstrap 3.0 compatible.
+* No need to paste shortcode in editor.
+* Add icons to editor.
+* Shortcode added to editor by clicking on icon.
+* Customize grid size on small, extra-small and medium sized screens.
+* More control over the css, you can add your own custom class to shortcodes.
+* Now you can use js file from CDN or disable the plugin js.
+* You can use your own css file by disabling the plugin css, which gives you more flexibility over styling.
+
+
+Easy Bootstrap Shortcode provides following styles
+
+= Grid Columns =
+1. Add multiple columns in a row
+2. Choose desired column width
+3. Add offset to columns
+4. Provide facility to customize a column according to medium, small and extra-small sized screens.
+5. Responsive columns
+
+= Shortcodes for =
+
+1. Accordion (Collapsible Menus)
+2. Tabs
+3. List
+4. Notification
+5. Popover
+6. Tooltip
+7. Panel
+8. Progress Bars
+9. Icons
+10. Labels
+11. Buttons
+12. Icon Heading
+13. Tables
+14. Responsive Image
+15. Image Effects (Round, Thumbnail, Square)
+16. Well
+17. Button Dropdown
+
+
+If you'd like to contribute to this plugin, you can find it [hosted on GitHub](https://github.com/oscitasthemes/Easy-Bootstrap-Shortcode).
+
+== Installation ==
+
+1.Upload:
+Unzip easy_bootstrap_shortcode.zip file, and upload all files to your blog folder: <root>/wp-content/plugins/easy_bootstrap_shortcode
+
+2.Activate:
+In the admin panel, find Plugins->Plugins,click it, active Easy Bootstrap Shortcode.
+
+3.Use:
+Now you can see the few new buttons in the TinyMCE Editor in post and pages, use these buttons to create great responsive pages/posts content.
+
+It's that simple ... just what the name suggests
+
+== Screenshots ==
+
+1. how it looks after installation
+2. different shortcode options this plugin provides
+3. columns shortcode
+4. table shortcode
+5. You can create this kind of page content very easily.
+6. mobile view
+7. few samples which can be created using this plugin
+8. sample 2 I
+9. sample 2 II
+10. sample 3
+11. sample 4 I
+12. sample 4 II
+
+
+== Changelog ==
+
+= 2.1.1 =
+
+
+* [Update] now use min files for bootstrap js/css files and css files for admin
+* [fixed] Button shortcode target(Open in new window) flag not working issue.
+* [fixed] tabs issue
+
+
+= 2.0.0 =
+
+* [New] New shortcodes for labels, progressbar, image effect, responsive image, well, dropdown button, icons
+* [New] Settings option to add bootstrap.js from CDN, disable plugin js/css file.
+* [Update] More control over the css, you can add your own custom class to shortcodes.
+* [Update] In columns shortcode now you can add different offsets for different columns for different screen sizes such as medium, small, ex-small.
+
+
+= 1.1 =
+* fixed columns alternate color issue in admin panel
+
---
+++ osc_bootstrap_shortcode.php
@@ -0,0 +1,111 @@
+<?php
+
+/*
+  Plugin Name: Easy Bootstrap Shortcode
+  Plugin URI: http://www.oscitasthemes.com
+  Description: Add bootstrap 3.0 styles to your theme by wordpress editor shortcode buttons.
+  Version: 2.1.1
+  Author: Oscitas Themes
+  Author URI: http://www.oscitasthemes.com
+  License: Under the GPL v2 or later
+ */
+register_activation_hook(__FILE__, 'osc_ebs_activate_plugin');
+register_deactivation_hook(__FILE__, 'osc_ebs_deactivate_plugin');
+add_action('admin_enqueue_scripts', 'osc_add_admin_ebs_scripts');
+add_action('wp_enqueue_scripts', 'osc_add_frontend_ebs_scripts');
+add_action('admin_menu', 'osc_ebs_add_admin_menu');
+
+function osc_ebs_activate_plugin() {
+    global $wpdb;
+    $sqlebs = "CREATE TABLE IF NOT EXISTS " . $wpdb->prefix . "ebs_settings (
+				id int(10) unsigned NOT NULL AUTO_INCREMENT,
+			  	ebs_js varchar(100),
+			  	ebs_css varchar(100),
+                                ebs_cdn_path varchar(500),
+                                PRIMARY KEY (id)
+                                )";
+
+    $wpdb->query($sqlebs);
+    $wpdb->insert(
+            $wpdb->prefix . 'ebs_settings', array(
+        'ebs_js' => 'plugin',
+        'ebs_css' => 'plugin',
+        'ebs_cdn_path' => 'http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js'
+            ), array(
+        '%s',
+        '%s',
+        '%s'
+            )
+    );
+}
+
+function osc_ebs_deactivate_plugin() {
+    global $wpdb;
+    $wpdb->query("DROP TABLE IF EXISTS " . $wpdb->prefix . "ebs_settings");
+}
+
+function osc_ebs_add_admin_menu() {
+    add_menu_page('EBS Settings', ' EBS Settings', 'manage_options', 'ebs/ebs-settings.php', 'osc_ebs_setting_page', plugins_url('/images/icon.png', __FILE__));
+}
+
+function osc_ebs_setting_page() {
+    global $wpdb;
+    $result = $wpdb->get_results('SELECT * from ' . $wpdb->prefix . "ebs_settings where id=1");
+    foreach ($result as $res) {
+        $js = $res->ebs_js;
+        $css = $res->ebs_css;
+        $cdn = $res->ebs_cdn_path;
+    }
+    include 'ebs_settings.php';
+    if (isset($_POST['ebs_submit'])) {
+//        $cdnins = ($_POST['b_js'] == 'cdn') ? $_POST['cdn_path'] : '';
+        $sqlins = 'UPDATE ' . $wpdb->prefix . 'ebs_settings set ebs_js="' . $_POST['b_js'] . '", ebs_css="' . $_POST['b_css'] . '", ebs_cdn_path="' . $_POST['cdn_path'] . '" where id=1';
+        $wpdb->query($sqlins);
+        echo ' <script type="text/javascript">window.location=document.URL;</script>';
+    }
+}
+
+//            add_submenu_page('optine
+function osc_add_admin_ebs_scripts() {
+    global $pagenow;
+    $screen = get_current_screen();
+    if ($screen->id == 'toplevel_page_ebs/ebs-settings') {
+        wp_enqueue_style('ebs-setting', plugins_url('/styles/ebs-setting.min.css', __FILE__));
+    }
+    if ('post-new.php' == $pagenow || 'post.php' == $pagenow) {
+        wp_enqueue_script('jquery');
+        wp_enqueue_style('thickbox');
+        wp_enqueue_script('media-upload');
+        wp_enqueue_script('thickbox');
+        wp_enqueue_script('jquery.fancybox', plugins_url('/js/jquery.fancybox.js', __FILE__));
+        wp_enqueue_style('fancyboxcss', plugins_url('/styles/jquery.fancybox.css', __FILE__));
+        wp_enqueue_style('bootstrap-icons', plugins_url('/styles/bootstrap-icon.min.css', __FILE__));
+        wp_enqueue_style('bootstrap_admin', plugins_url('/styles/bootstrap_admin.min.css', __FILE__));
+    }
+}
+
+function osc_add_frontend_ebs_scripts() {
+    wp_enqueue_script('jquery');
+    global $wpdb;
+    $result = $wpdb->get_results('SELECT * from ' . $wpdb->prefix . "ebs_settings where id=1");
+    foreach ($result as $res) {
+        $js = $res->ebs_js;
+        $css = $res->ebs_css;
+        $cdn = $res->ebs_cdn_path;
+    }
+    if ($js == 'plugin') {
+        wp_enqueue_script('bootstrap', plugins_url('/js/bootstrap.min.js', __FILE__));
+    } elseif ($js == 'cdn') {
+        wp_enqueue_script('bootstrap', $cdn);
+    }
+    if ($css == 'plugin') {
+        wp_enqueue_style('bootstrap', plugins_url('/styles/bootstrap.min.css', __FILE__));
+    } else {
+        wp_enqueue_style('bootstrap-icon', plugins_url('/styles/bootstrap-icon.min.css', __FILE__));
+    }
+}
+
+// Shortcodes
+include('shortcode/functions.php');
+
+
