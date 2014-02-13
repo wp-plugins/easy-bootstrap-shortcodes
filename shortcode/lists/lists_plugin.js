d6347357c9a10c9820763da071b4c112334b8371
oscitas <oscitas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 20:12:21 +0000

    fix : List Shortcode not working
    git-svn-id: https://plugins.svn.wordpress.org/easy-bootstrap-shortcodes/trunk@857452 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcode/lists/lists_plugin.js
+++ shortcode/lists/lists_plugin.js
@@ -11,10 +11,7 @@ function create_oscitas_lists(pluginObj){
     if(jQuery(pluginObj.hashId).length){
         jQuery(pluginObj.hashId).remove();
     }
-    var icon_fa='';
-    if(ebs.ebs_fa_inclusion==1){
-        icon_fa='<h4>Font Awesome</h4><ul name="oscitas-heading-icon_icon" class="oscitas-heading-icon_icon">'+ebsfaicons+'</ul>';
-    }
+
 
     // creates a form to be displayed everytime the button is clicked
     // you should achieve this using AJAX instead of direct html code like this
