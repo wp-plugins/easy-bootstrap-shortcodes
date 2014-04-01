5f882bcd184f1d0300c5a097182dc3fddad16ab0
oscitas <oscitas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-01 13:25:31 +0000

    = 3.5.0 =
    * [Fixes] Custom css was not working for non bootstrap themes issue resolved.
    * [Fixes] Session warring issue in service box shortcode.
    
    git-svn-id: https://plugins.svn.wordpress.org/easy-bootstrap-shortcodes/trunk@885618 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- styles/ebs_dynamic_css.php
+++ styles/ebs_dynamic_css.php
@@ -3,7 +3,9 @@ header("Content-type: text/css");
 if(!session_id())
     session_start();
 echo $_SESSION['ebs_dynamic_css'];
-
+if(isset($_SESSION['ebs_dynamic_css'])){
+    echo $_SESSION['ebs_dynamic_css'];
+}
 
 if(isset($_SESSION['ebs_slider_css'])){
     foreach($_SESSION['ebs_slider_css'] as $val){
